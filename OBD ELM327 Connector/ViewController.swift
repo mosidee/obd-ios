166d25a27e14d20a51c2daace12b7516116caf54
mosidee <bhumsonj@gmail.com>
2026-05-24 23:09:00 +0700

    Initial Commit

---
+++ OBD ELM327 Connector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OBD ELM327 Connector
+//
+//  Created by Bhumson Jaturasukumal on 24/5/2569 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
